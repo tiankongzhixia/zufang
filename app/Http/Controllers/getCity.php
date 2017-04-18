17ca7dfd489db9c84de58aff52c97ba2427a8514
guoweijie <guoweijie@36kr.com>
2017-04-18 14:30:16 +0800

    a

--- app/Http/Controllers/getCity.php
+++ app/Http/Controllers/getCity.php
@@ -8,12 +8,13 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
-
+use Illuminate\Http\Response;
 class getCity
 {
     public function getCity(){
         $query = DB::table('region')->select('name as region')
            ->where('superior_city','北京')->get();
-        return $query;
+        return Response::json(['code' => 0, 'data' => ['info' => $query]]);
+
     }
 }
