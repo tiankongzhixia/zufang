5a08fad47a18c21b9ef8a1bbc4a405e7de4a6081
guoweijie <guoweijie@36kr.com>
2017-04-18 16:27:36 +0800

    对对对

--- app/Http/Controllers/getCity.php
+++ app/Http/Controllers/getCity.php
@@ -14,7 +14,7 @@ class getCity
     public function getCity(){
         $city = DB::table('region')->where('superior_city','北京')->pluck('name')->toArray();
         $region = DB::table('region2')->join('region','region.name','=','region2.region')
-            ->select('region2.*');
+            ->select('region2.*')->get();
 //        $list = array();
 //        foreach ($city as $item) {
 //            $region2 = DB::table('users')
