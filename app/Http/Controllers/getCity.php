7d09fa36beb728901d1480ff5a02993c5a2a5569
guoweijie <guoweijie@36kr.com>
2017-04-18 18:20:20 +0800

    dsd

--- app/Http/Controllers/getCity.php
+++ app/Http/Controllers/getCity.php
@@ -8,21 +8,31 @@
 
 namespace App\Http\Controllers;
 use DB;
+use Cache;
 
 class getCity
 {
     public function getCity(){
-        $city = DB::table('region')->where('superior_city','北京')->pluck('name')->toArray();
-        $list=array();
-        foreach ($city as $item) {
-            $response = [$item => DB::table('region2')->join('region','region.name','=','region2.region')
-                ->select('region2.*')
-                ->where('region',$item)
-                ->pluck('name')];
-            array_push($list,$response);
+        $rd = array();
+
+        if(Cache::has('city')){
+            $rd = Cache::get('city');
+        }else {
+            $city = DB::table('region')->where('superior_city', '北京')->pluck('name')->toArray();
+            $list = array();
+            foreach ($city as $item) {
+                $response = [$item => DB::table('region2')->join('region', 'region.name', '=', 'region2.region')
+                    ->select('region2.*')
+                    ->where('region', $item)
+                    ->pluck('name')];
+                var_dump($response);
+                array_push($list, $response);
+            }
+            $rd =$list;
+            Cache::put('city', $rd, 10);
         }
 
-        return response()->json(['code' => 0, 'data' => ['北京'=>$list]]);
+        return response()->json(['code' => 0, 'data' => ['北京'=>$rd]]);
 
     }
 }
