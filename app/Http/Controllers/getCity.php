b0cd4b4318c18f79c15fceba9891fae2d811bdc5
guoweijie <guoweijie@36kr.com>
2017-04-18 15:14:45 +0800

    fasa

--- app/Http/Controllers/getCity.php
+++ app/Http/Controllers/getCity.php
@@ -12,7 +12,7 @@ use DB;
 class getCity
 {
     public function getCity(){
-        $query = DB::table('region')->pluck('name');
+        $query = DB::table('region')->where('superior_city','北京')->pluck('name');
 
         return response()->json(['code' => 0, 'data' => ['北京'=>$query]]);
 
