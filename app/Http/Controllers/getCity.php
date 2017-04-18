e2bb64abd9a31b2e7326df6d69161a4a587d7d02
guoweijie <guoweijie@36kr.com>
2017-04-18 16:19:44 +0800

    sfdsf

--- app/Http/Controllers/getCity.php
+++ app/Http/Controllers/getCity.php
@@ -12,10 +12,20 @@ use DB;
 class getCity
 {
     public function getCity(){
-        //$city = DB::table('region')->where('superior_city','北京')->pluck('name');
+        $city = DB::table('region')->where('superior_city','北京')->pluck('name')->toArray();
         $region = DB::table('region2')->join('region','region.name','=','region2.region')
-            ->select('region2.*')->pluck('name');
-        return response()->json(['code' => 0, 'data' => ['北京'=>$region]]);
+            ->select('region2.*');
+        $list = array();
+        foreach ($city as $item) {
+            $region2 = DB::table('users')
+                ->where('region')
+                ->union($region,$item)
+                ->pluck('name');
+            $response = [$item => $region2];
+            Array_push($list,$response);
+        }
+
+        return response()->json(['code' => 0, 'data' => ['北京'=>$list]]);
 
     }
 }
