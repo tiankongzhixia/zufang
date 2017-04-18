4a5ac1c65241df11353d8a0abd2177c4f9fb7a30
guoweijie <guoweijie@36kr.com>
2017-04-18 15:58:07 +0800

    asda

--- app/Http/Controllers/getCity.php
+++ app/Http/Controllers/getCity.php
@@ -12,9 +12,9 @@ use DB;
 class getCity
 {
     public function getCity(){
-        $query = DB::table('region')->where('superior_city','北京')->pluck('name');
-
-        return response()->json(['code' => 0, 'data' => ['北京'=>$query]]);
+        //$city = DB::table('region')->where('superior_city','北京')->pluck('name');
+        $region = DB::table('region2')->join('region', 'region2.region', '=', 'region.name')->where('region','朝阳')->pluck('name');
+        return response()->json(['code' => 0, 'data' => ['北京'=>$region]]);
 
     }
 }
